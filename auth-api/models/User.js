322a5364fd46953a2566743a85cd4eab7f630b77
Ani Sudhakar <anithasth92@gmail.com>
2026-03-28 06:36:32 +0000

    update

--- auth-api/models/User.js
+++ auth-api/models/User.js
@@ -18,15 +18,12 @@ const userSchema = new mongoose.Schema({
   }
 }, { timestamps: true });
 
-// Hash password before saving
 userSchema.pre('save', async function () {
-  if (!this.isModified('password')) return;  
+  if (!this.isModified('password')) return;
   this.password = await bcrypt.hash(this.password, 12);
 });
 
-// Compare password method
 userSchema.methods.comparePassword = async function (candidate) {
   return bcrypt.compare(candidate, this.password);
 };
-
 module.exports = mongoose.model('User', userSchema);
